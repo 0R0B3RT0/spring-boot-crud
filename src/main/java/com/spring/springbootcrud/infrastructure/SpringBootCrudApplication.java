9f28c7149eae3f0af42b8a15a499e5d12cd9a5b9
Roberto Candido de Oliveira Junior <roberto.candido@contaazul.com>
2019-10-12 18:12:23 -0300

    fix(SpringBootCrudApplication): Adicionada as anotações @EntityScan, @EnableJpaRepositories e configuração do scanBasePackages da anotação @SpringBootApplication

--- src/main/java/com/spring/springbootcrud/infrastructure/SpringBootCrudApplication.java
+++ src/main/java/com/spring/springbootcrud/infrastructure/SpringBootCrudApplication.java
@@ -2,8 +2,12 @@ package com.spring.springbootcrud.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EntityScan(basePackages = "com.spring.springbootcrud.domain.entity")
+@EnableJpaRepositories(basePackages = "com.spring.springbootcrud.domain.repository")
+@SpringBootApplication(scanBasePackages = "com.spring.springbootcrud")
 public class SpringBootCrudApplication {
 
   public static void main(String[] args) {
