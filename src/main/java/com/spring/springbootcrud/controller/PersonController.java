3707177f063ed1df9c3e5b88128e353c06f0bcff
Roberto Candido de Oliveira Junior <roberto.candido@contaazul.com>
2019-10-12 19:17:39 -0300

    feat(PersonController): Adicionando command hystrix no endpoit de cadastro de pessoa

--- src/main/java/com/spring/springbootcrud/controller/PersonController.java
+++ src/main/java/com/spring/springbootcrud/controller/PersonController.java
@@ -2,6 +2,9 @@ package com.spring.springbootcrud.controller;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.spring.springbootcrud.domain.dto.PersonDTO;
 import com.spring.springbootcrud.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@DefaultProperties(
+    groupKey = "person",
+    commandProperties =
+        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "800"))
 @Validated
 @RestController
 @RequestMapping(value = "person", produces = APPLICATION_JSON_VALUE)
@@ -20,6 +27,7 @@ public class PersonController {
   @Autowired private PersonService personService;
 
   @PostMapping
+  @HystrixCommand(commandKey = "person_save-new")
   public ResponseEntity<PersonDTO> saveNew(@RequestBody PersonDTO personDTO) {
     return ResponseEntity.ok(personService.save(personDTO));
   }
