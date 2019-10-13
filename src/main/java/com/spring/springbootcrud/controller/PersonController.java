3a405c9cc49163f821eb6b87fa758a2ab3ce5811
Roberto Candido de Oliveira Junior <roberto.candido@contaazul.com>
2019-10-13 00:34:43 -0300

    feat(PersonController): Criação do endpoint buscar todas pessoas ativas

--- src/main/java/com/spring/springbootcrud/controller/PersonController.java
+++ src/main/java/com/spring/springbootcrud/controller/PersonController.java
@@ -2,18 +2,22 @@ package com.spring.springbootcrud.controller;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.spring.springbootcrud.domain.dto.PersonDTO;
 import com.spring.springbootcrud.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @DefaultProperties(
     groupKey = "person",
@@ -27,8 +31,14 @@ public class PersonController {
   @Autowired private PersonService personService;
 
   @PostMapping
-  @HystrixCommand(commandKey = "person_save-new")
+  @HystrixCommand(commandKey = "save-new")
   public ResponseEntity<PersonDTO> saveNew(@RequestBody PersonDTO personDTO) {
     return ResponseEntity.ok(personService.save(personDTO));
+  }
+
+  @GetMapping
+  @HystrixCommand(commandKey = "find-enabled-people")
+  public ResponseEntity<List<PersonDTO>> findEnabledPeople() {
+    return ResponseEntity.ok(personService.findEnabledPeople());
   }
 }
--- src/test/java/com/spring/springbootcrud/controller/PersonControllerTest.java
+++ src/test/java/com/spring/springbootcrud/controller/PersonControllerTest.java
@@ -1,20 +1,26 @@
 package com.spring.springbootcrud.controller;
 
+import static org.assertj.core.util.Lists.newArrayList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpStatus.OK;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.ResponseEntity;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.spring.springbootcrud.BaseUnitTest;
 import com.spring.springbootcrud.domain.dto.PersonDTO;
 import com.spring.springbootcrud.service.PersonService;
-import java.lang.reflect.Method;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.http.ResponseEntity;
 
 public class PersonControllerTest extends BaseUnitTest {
 
@@ -24,12 +30,14 @@ public class PersonControllerTest extends BaseUnitTest {
   public void setup() {
     super.setup();
     when(personService.save(personDTO)).thenReturn(expectedPersonDTO);
+    when(personService.findEnabledPeople()).thenReturn(newArrayList(expectedPersonDTO));
   }
 
   @Test
-  public void mustHttpStatusOKWhenHasPersonDTOValid() {
+  public void mustHttpStatusOKWhenSavePersonDTOValid() {
     final ResponseEntity<PersonDTO> actualPersonDTO = personController.saveNew(personDTO);
 
+    verify(personService, only()).save(personDTO);
     assertThat(actualPersonDTO.getStatusCode(), equalTo(OK));
     assertAllAttributesOfPersonDTO(actualPersonDTO.getBody());
   }
@@ -41,6 +49,25 @@ public class PersonControllerTest extends BaseUnitTest {
     final HystrixCommand annotation = getAnnotation(method, HystrixCommand.class);
 
     assertThat(annotation, notNullValue());
-    assertThat(annotation.commandKey(), equalTo("person_save-new"));
+    assertThat(annotation.commandKey(), equalTo("save-new"));
+  }
+
+  @Test
+  public void mustHttpStatusOKWhenFindEnabledPeople() {
+    final ResponseEntity<List<PersonDTO>> actualPersonDTO = personController.findEnabledPeople();
+
+    verify(personService, only()).findEnabledPeople();
+    assertThat(actualPersonDTO.getStatusCode(), equalTo(OK));
+    assertAllAttributesOfPersonDTO(actualPersonDTO.getBody().get(0));
+  }
+
+  @Test
+  public void findEnabledPeopleMustBeAnnotatedWithHystrixCommand() {
+    final Method method = getMethod(PersonController.class, "findEnabledPeople");
+
+    final HystrixCommand annotation = getAnnotation(method, HystrixCommand.class);
+
+    assertThat(annotation, notNullValue());
+    assertThat(annotation.commandKey(), equalTo("find-enabled-people"));
   }
 }
